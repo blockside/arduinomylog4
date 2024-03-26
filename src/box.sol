a88c3e4b9c0a2e295416d83bb8d942d630463099
blockside <63253094+blockside@users.noreply.github.com>
2024-03-26 15:11:51 +0100

    box.sol

--- src/box.sol
+++ src/box.sol
@@ -1,1 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.22;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Box is Ownable {
+   constructor() Ownable(msg.sender) {}
+   uint256 private _value;
+    event ValueChanged(uint256 value);
+    function store(uint256 value) public onlyOwner {
+        _value = value;
+        emit ValueChanged(value);
+    }
+    function retrieve() public view returns (uint256) {
+        return _value;
+    }
+}
